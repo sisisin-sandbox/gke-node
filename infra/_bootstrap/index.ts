b87657e002e5eed75e0de44fe94a1e68702751da
sisisin <azsisisin@gmail.com>
2022-09-24 21:44:25 +0900

    fix

--- infra/_bootstrap/index.ts
+++ infra/_bootstrap/index.ts
@@ -45,10 +45,13 @@ new gcp.iam.WorkloadIdentityPoolProvider('ci-pool-oidc-provider', {
   //   assertion.job_workflow_ref.startsWith("${repo}/.github/workflows/deploy-infra-bootstrap.yaml")
   // `,
   attributeCondition: `
-    assertion.job_workflow_ref.startsWith("${repo}/.github/workflows/preview-infra-main.yaml") ||
-    assertion.job_workflow_ref.startsWith("${repo}/.github/workflows/deploy-infra-main.yaml") ||
-    assertion.job_workflow_ref.startsWith("${repo}/.github/workflows/preview-infra-bootstrap.yaml") ||
-    assertion.job_workflow_ref.startsWith("${repo}/.github/workflows/deploy-infra-bootstrap.yaml")
+    assertion.job_workflow_ref.startsWith("${repo}/.github/workflows) &&
+    (
+      assertion.job_workflow_ref.contains("preview-infra-main.yaml") ||
+      assertion.job_workflow_ref.contains("deploy-infra-main.yaml") ||
+      assertion.job_workflow_ref.contains("preview-infra-bootstrap.yaml") ||
+      assertion.job_workflow_ref.contains("deploy-infra-bootstrap.yaml")
+    )
   `,
 });
 
