78ba3a1f808f9bbe9835fca08db25aa2fadf542a
sisisin <azsisisin@gmail.com>
2022-09-24 22:10:30 +0900

    fix

--- infra/_bootstrap/index.ts
+++ infra/_bootstrap/index.ts
@@ -39,19 +39,17 @@ new gcp.iam.WorkloadIdentityPoolProvider('ci-pool-oidc-provider', {
     'google.subject': 'assertion.sub',
     'attribute.actor': 'assertion.actor',
     'attribute.aud': 'assertion.aud',
+    'attribute.repository': 'assertion.repository',
   },
   // attributeCondition: `
   //   assertion.job_workflow_ref.startsWith("${repo}/.github/workflows/preview-infra-bootstrap.yaml") ||
   //   assertion.job_workflow_ref.startsWith("${repo}/.github/workflows/deploy-infra-bootstrap.yaml")
   // `,
   attributeCondition: `
-    assertion.job_workflow_ref.startsWith("${repo}/.github/workflows) &&
-    (
-      assertion.job_workflow_ref.contains("preview-infra-main.yaml") ||
-      assertion.job_workflow_ref.contains("deploy-infra-main.yaml") ||
-      assertion.job_workflow_ref.contains("preview-infra-bootstrap.yaml") ||
-      assertion.job_workflow_ref.contains("deploy-infra-bootstrap.yaml")
-    )
+    assertion.job_workflow_ref.startsWith("${repo}/.github/workflows/preview-infra-main.yaml) ||
+    assertion.job_workflow_ref.startsWith("${repo}/.github/workflows/deploy-infra-main.yaml") ||
+    assertion.job_workflow_ref.startsWith("${repo}/.github/workflows/preview-infra-bootstrap.yaml") ||
+    assertion.job_workflow_ref.startsWith("${repo}/.github/workflows/deploy-infra-bootstrap.yaml")
   `,
 });
 
